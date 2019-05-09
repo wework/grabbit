9e5407bb675d5070df1155d3ac7a32737bafdd56
Guy Baron <guy.baron@wework.com>
2019-05-09 17:52:04 +0300

    added tests for protobuf serializer error scenarios

--- tests/protoSerialization_test.go
+++ tests/protoSerialization_test.go
@@ -8,34 +8,46 @@ import (
 	"github.com/wework/grabbit/gbus/serialization"
 )
 
+func getProtoCommand() *ProtoCommand {
+	return &ProtoCommand{
+		SomeNumber: 15,
+		SomeData:   "rhinof"}
+}
+
 func TestProtoSerialization(t *testing.T) {
-
+	// so the tests logs do not get littered with log entries from serlializer
+	log.SetLevel(log.PanicLevel)
+	defer log.SetLevel(log.InfoLevel)
 	logger := log.WithField("test", "proto_serialization")
-	serializer := serialization.NewProtoSerializer(logger)
-	cmd := &ProtoCommand{}
-	schemaName := cmd.SchemaName()
-	cmd.SomeNumber = 15
-	cmd.SomeData = "rhinof"
-	encodedBytes, encErr := serializer.Encode(cmd)
 
+	serializer := serialization.NewProtoSerializer(logger)
+
+	name := serializer.Name()
+	if name != "proto" {
+		t.Fatalf("incorrect serializer name. expected:proto actual:%s", name)
+	}
+	cmd := getProtoCommand()
+	schemaName := cmd.SchemaName()
+
+	encodedBytes, encErr := serializer.Encode(cmd)
 	if encErr != nil {
-		t.Errorf("encoding returned an error: %v", encErr)
+		t.Fatalf("encoding returned an error: %v", encErr)
 	}
 
 	//Calling Decode with out first registering the schema should fail and return an error
 	_, decErr := serializer.Decode(encodedBytes, schemaName)
 	if decErr == nil {
-		t.Error("decoding expected to fail but did not return an error")
+		t.Fatalf("decoding expected to fail but did not return an error")
 	}
 
 	serializer.Register(cmd)
+
 	decodedMsg, noErr := serializer.Decode(encodedBytes, schemaName)
 	if noErr != nil {
-		t.Errorf("decoding of message failed with error:%v", noErr)
+		t.Fatalf("decoding of message failed with error:%v", noErr)
 	}
 
 	cmdCopy, ok := decodedMsg.(*ProtoCommand)
-
 	if !ok {
 		t.Errorf("decoded message was of wrong type. expected:%v actual:%v", reflect.TypeOf(cmd), reflect.TypeOf(decodedMsg))
 	}
@@ -44,6 +56,33 @@ func TestProtoSerialization(t *testing.T) {
 		log.Infof("expected:%v\n", cmd)
 		log.Infof("actual:%v\n", cmdCopy)
 		t.Errorf("decoded message has unexpected or missing data")
+	}
+
+}
+
+func TestProtoSerializationErrors(t *testing.T) {
+	// so the tests logs do not get littered with log entries from serlializer
+	log.SetLevel(log.PanicLevel)
+	defer log.SetLevel(log.InfoLevel)
+	logger := log.WithField("test", "proto_serialization")
+
+	serializer := serialization.NewProtoSerializer(logger)
+
+	// test that encoding a non protobuf generated strcut fails and returns an error
+	_, encErr := serializer.Encode(Command1{})
+	if encErr == nil {
+		t.Errorf("serializer expected to return an error for non proto generated messages but di not")
+	}
+
+	cmd := getProtoCommand()
+	encodedBytes, encErr := serializer.Encode(cmd)
+	if encErr != nil {
+		t.Fatalf("encoding returned an error: %v", encErr)
+	}
+
+	//decoding an unregistered schema fails and returns an error
+	if _, decErr := serializer.Decode(encodedBytes, "kong"); decErr == nil {
+		t.Errorf("decoding an unregistred schema  is expected to return an error but did not")
 	}
 
 }
