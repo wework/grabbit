75458f01802b5acbf2b6c5f815e70d9c45f646e5
Guy Baron <guy.baron@wework.com>
2019-08-17 16:12:34 +0300

    added comments on exported types

--- gbus/message_handler.go
+++ gbus/message_handler.go
@@ -9,6 +9,7 @@ import (
 //MessageHandler signature for all command handlers
 type MessageHandler func(invocation Invocation, message *BusMessage) error
 
+//Name is a helper function returning the runtime name of the function bound to an instance of the MessageHandler type
 func (mg MessageHandler) Name() string {
 	funName := runtime.FuncForPC(reflect.ValueOf(mg).Pointer()).Name()
 	splits := strings.Split(funName, ".")
