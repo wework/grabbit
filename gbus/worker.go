af8f8991bd1da2946f25750d2cb15af580ac6254
Yuval Mendelbaum <yuval.mendelbaum@wework.com>
2019-10-16 10:53:01 +0300

    moved getRoutingParamsFromDelivery to worker.go

--- gbus/worker.go
+++ gbus/worker.go
@@ -172,6 +172,26 @@ func (worker *worker) resolveHandlers(isRPCreply bool, delivery amqp.Delivery) [
 	return handlers
 }
 
+func getRoutingParamsFromDelivery(delivery amqp.Delivery) (exchange string, routingKey string, err error) {
+
+	// In order to route "resurrected" events to the correct handler it is necessary to use the original exchange and routing-key which
+	// were added by grabbit previously as headers, as opposed to the native values on the delivery
+	// Issue reference:	https://github.com/wework/grabbit/issues/191
+	if isResurrectedMessage(delivery) {
+		exchange, ok := delivery.Headers["x-first-death-exchange"].(string)
+		if !ok {
+			return "", "", errors.New("failed extracting exchange from resurrected message, bad x-first-death-exchange")
+		}
+		routingKey, ok := delivery.Headers[FirstDeathRoutingKeyHeaderName].(string)
+		if !ok {
+			return "", "", errors.New("failed extracting routing-key from resurrected message, bad x-first-death-routing-key")
+		}
+		return exchange, routingKey, nil
+	}
+
+	return delivery.Exchange, delivery.RoutingKey, nil
+}
+
 func (worker *worker) ack(delivery amqp.Delivery) error {
 	ack := func(attempts uint) error { return delivery.Ack(false /*multiple*/) }
 	worker.log().WithField("message_id", delivery.MessageId).Debug("acking message")
